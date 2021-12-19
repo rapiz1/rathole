b8e824849a433aee4f08c3dd68fe463b16738ba3
Yujia Qiao <rapiz3142@gmail.com>
2021-12-19 11:31:58 +0800

    refactor: facilitate tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use anyhow::Result;
 use clap::Parser;
 use rathole::{run, Cli};
+use tokio::{signal, sync::broadcast};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Cli::parse();
-    run(&args).await
+
+    let (shutdown_tx, shutdown_rx) = broadcast::channel::<bool>(1);
+    tokio::spawn(async move {
+        if let Err(e) = signal::ctrl_c().await {
+            // Something really weird happened. So just panic
+            panic!("Failed to listen for the ctrl-c signal: {:?}", e);
+        }
+
+        if let Err(e) = shutdown_tx.send(true) {
+            // shutdown signal must be catched and handle properly
+            // `rx` must not be dropped
+            panic!("Failed to send shutdown signal: {:?}", e);
+        }
+    });
+
+    // TODO: use level from config
+    tracing_subscriber::fmt::init();
+
+    run(&args, shutdown_rx).await
 }
