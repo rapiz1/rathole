8a86b9c571e1472d51bd84352d8c507032891cf4
Yujia Qiao <rapiz3142@gmail.com>
2022-01-03 17:50:28 +0800

    fix: panic on top-level errors

--- src/lib.rs
+++ src/lib.rs
@@ -84,7 +84,7 @@ pub async fn run(args: Cli, shutdown_rx: broadcast::Receiver<bool>) -> Result<()
                 if let Some((i, _)) = last_instance {
                     info!("General configuration change detected. Restarting...");
                     shutdown_tx.send(true)?;
-                    i.await??;
+                    i.await?;
                 }
 
                 debug!("{:?}", config);
@@ -120,8 +120,8 @@ async fn run_instance(
     args: Cli,
     shutdown_rx: broadcast::Receiver<bool>,
     service_update: mpsc::Receiver<ServiceChange>,
-) -> Result<()> {
-    match determine_run_mode(&config, &args) {
+) {
+    let ret: Result<()> = match determine_run_mode(&config, &args) {
         RunMode::Undetermine => panic!("Cannot determine running as a server or a client"),
         RunMode::Client => {
             #[cfg(not(feature = "client"))]
@@ -135,7 +135,8 @@ async fn run_instance(
             #[cfg(feature = "server")]
             run_server(&config, shutdown_rx, service_update).await
         }
-    }
+    };
+    ret.unwrap();
 }
 
 #[derive(PartialEq, Eq, Debug)]
